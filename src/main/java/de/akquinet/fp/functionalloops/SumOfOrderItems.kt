0b555ee68ed38cf16137b50374f988bf651ac549
Torsten Fink <torsten.fink@akquinet.de>
2019-06-14 17:38:03 +0200

    first imperative sum

---
+++ src/main/java/de/akquinet/fp/functionalloops/SumOfOrderItems.kt
@@ -0,0 +1,11 @@
+package de.akquinet.fp.functionalloops
+
+data class OrderItem (val orderNumber: String, val amount: Int )
+
+fun sumImperativ(items : List<OrderItem>) : Int {
+    var result = 0
+    for( i in 0 until items.size) {
+        result += items[i].amount
+    }
+    return result
+}
---
+++ src/test/java/de/akquinet/fp/functionalloops/SumOfOrderItemsKtTest.kt
@@ -0,0 +1,18 @@
+package de.akquinet.fp.functionalloops
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class SumOfOrderItemsKtTest {
+
+    private val testItems = listOf(
+            OrderItem("OE1234", 10),
+            OrderItem("OE56789",15))
+
+    private val sumOfOrderedItems = 25
+
+    @Test
+    fun sumImperativ() {
+        assertEquals(sumOfOrderedItems, sumImperativ(testItems))
+    }
+}
