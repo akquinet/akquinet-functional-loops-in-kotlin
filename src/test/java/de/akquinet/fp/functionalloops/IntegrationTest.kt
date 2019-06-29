677bf49765a44846ef8266afd8b1d23369337618
Torsten Fink <torsten.fink@akquinet.de>
2019-06-29 10:12:00 +0200

    did some measurements

--- src/test/java/de/akquinet/fp/functionalloops/IntegrationTest.kt
+++ src/test/java/de/akquinet/fp/functionalloops/IntegrationTest.kt
@@ -26,11 +26,9 @@ internal class IntegrationTest {
         testIntegrationFunction(::integrateFunctionalSequence2)
     }
 
-    private val iterations = 100
+    private val iterations = 10000000
 
     private fun testIntegrationFunction(integrationF: IntegrationType) {
-        println("$Int.MAX_VALUE $iterations ${Int.MAX_VALUE-iterations} " + Int.MAX_VALUE)
-
         val const1: (Double) -> Double = { _ -> 1.0 }
         //assertNearlyEquals(1.0, integrationF(0.0, 1.0, iterations, const1))
         //assertNearlyEquals(1.0, integrationF(0.0, 1.0, iterations *1, const1))
@@ -38,7 +36,7 @@ internal class IntegrationTest {
     }
 
     fun assertNearlyEquals( expected : Double, actual : Double) {
-        val allowedError = 0.001
+        val allowedError = 0.00000001
         val delta = (expected - actual).absoluteValue
         val message = "expected $expected differs from actual $actual, delta is $delta"
         assertTrue(delta < allowedError, message)
