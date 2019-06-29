73c2c7496860da1c8db0d951a74134527ff84338
Torsten Fink <torsten.fink@akquinet.de>
2019-06-29 13:31:37 +0200

    minor optimisation

--- src/main/java/de/akquinet/fp/functionalloops/Integration.kt
+++ src/main/java/de/akquinet/fp/functionalloops/Integration.kt
@@ -1,29 +1,31 @@
 package de.akquinet.fp.functionalloops
 
+import kotlin.system.measureTimeMillis
+
 typealias IntegrationType = (Double, Double, Int, (Double) -> Double) -> Double
 
-fun integrateImperative(start :Double, end : Double, precision: Int,
-                        f : (Double) -> Double) : Double {
-    val step = (end-start) / precision
+fun integrateImperative(start: Double, end: Double, precision: Int,
+                        f: (Double) -> Double): Double {
+    val step = (end - start) / precision
     var result = 0.0
     var x = start
-    for ( i in 0 until precision) {
+    for (i in 0 until precision) {
         result += f(x) * step
         x += step
     }
     return result
 }
 
-fun integrateFunctional(start :Double, end : Double, precision: Int, f : (Double) -> Double) : Double {
-    val step = (end-start) / precision
-    return ( 0 until precision)
-            .map { index -> start + index * step}
+fun integrateFunctional(start: Double, end: Double, precision: Int, f: (Double) -> Double): Double {
+    val step = (end - start) / precision
+    return (0 until precision)
+            .map { index -> start + index * step }
             .map { x -> f(x) * step }
             .sum()
 }
 
-fun integrateFunctionalCleanCode(start :Double, end : Double, precision: Int, f : (Double) -> Double) : Double {
-    val step = (end-start) / precision
+fun integrateFunctionalCleanCode(start: Double, end: Double, precision: Int, f: (Double) -> Double): Double {
+    val step = (end - start) / precision
     val xCoordinates = (0 until precision)
             .map { index -> start + index * step }
     val allRectangles = xCoordinates
@@ -32,10 +34,10 @@ fun integrateFunctionalCleanCode(start :Double, end : Double, precision: Int, f 
             .sum()
 }
 
-fun integrateFunctionalSequence(start :Double, end : Double, precision: Int, f : (Double) -> Double) : Double {
-    val step = (end-start) / precision
-    return ( 0 until precision).asSequence()
-            .map { index -> start + index * step}
+fun integrateFunctionalSequence(start: Double, end: Double, precision: Int, f: (Double) -> Double): Double {
+    val step = (end - start) / precision
+    return (0 until precision).asSequence()
+            .map { index -> start + index * step }
             .map { x -> f(x) * step }
             .sum()
 }
@@ -44,11 +46,45 @@ fun integrateFunctionalSequence(start :Double, end : Double, precision: Int, f :
 fun integrateFunctionalSequence2(start :Double, end : Double, precision: Long, f : (Double) -> Double) : Double {
  produces a set of step +start, 2*step + start, ...
 */
-fun integrateFunctionalSequence2(start :Double, end : Double, precision: Int, f : (Double) -> Double) : Double {
-    val step = (end-start) / precision
-    return generateSequence(start) { x -> x + step}
+fun integrateFunctionalSequence2(start: Double, end: Double, precision: Int, f: (Double) -> Double): Double {
+    val step = (end - start) / precision
+    return generateSequence(start) { x -> x + step }
             .map { x -> f(x) * step }
             .take(precision)
             .sum()
 }
 
+fun main() {
+    println("Starting benchmark")
+
+    fun pow10(x: Int): Int =
+            (1 until x).fold(10, { acc, _ -> acc * 10 })
+
+    fun benchmark(precision: Int, f: IntegrationType) {
+        val numberOfRepetitions = 5
+        fun testFunction(x: Double): Double = 1.0
+        System.gc()
+        val averageExecutionTimeNs = (0..numberOfRepetitions)
+                .map {
+                    try { measureTimeMillis { f(-1.0, 1.0, precision, ::testFunction) } }
+                    catch(e: Throwable) {Long.MIN_VALUE}
+                }
+                .average()
+        println("precision $precision: average $averageExecutionTimeNs")
+    }
+
+    val precisions = (4..8).map(::pow10)
+    val functions = listOf(
+            ::integrateImperative
+            , ::integrateFunctional
+            , ::integrateFunctionalCleanCode
+            , ::integrateFunctionalSequence
+            , ::integrateFunctionalSequence2)
+
+    functions.forEach { f ->
+        println("benchmarking function $f")
+        precisions.forEach { precision ->
+            benchmark(precision, f)
+        }
+    }
+}
